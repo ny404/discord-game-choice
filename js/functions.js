5564de3eded8b3ddca0c78dc5234546464a1eab4
ny404 <nicholas.m.yeung@gmail.com>
2020-10-15 12:48:20 -0400

    added button disability that works with timer

--- js/functions.js
+++ js/functions.js
@@ -9,20 +9,34 @@ function timer()
   {
      clearInterval(counter);
      //counter ended, do something here
+     enableButton();
      return;
   }
+  if (count > 0) { disableButton(); }
   console.log(count);
   //Do code for showing the number of seconds here
 }
 
 function disableButton() {
-  y = document.getElementsByClassName("myBtn");
+  y = document.getElementsByClassName("pink");
   for (i = 0; i < y.length; i++) {
     y[i].disabled = true;
+    y[i].style.backgroundColor = "#666";
+    y[i].innerHTML = "Wait " + count + " sec.";
+  }
+}
+
+function enableButton() {
+  y = document.getElementsByClassName("pink");
+  for (i = 0; i < y.length; i++) {
+    y[i].disabled = false;
+    y[i].removeAttribute("style");
+    y[i].innerHTML = "Play This Game";
   }
 }
 
 function loadStatus() {
+  disableButton();
     // ID of the Google Spreadsheet
   var spreadsheetID = '1poYa0UrCWgFU4I39rJ1jg3twIAthc02SmvisUzgftN4';
 
@@ -47,8 +61,8 @@ function loadStatus() {
 function sendMessage(x,img) {
   if (count <= 0) {
     var request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/762084461092798494/3h6pWBiM9JW94CKIMBreK9dHpEO7AZyE9XCLDVg12xcZG1EGz19uSUw9BCqLuea9Mb6H");
-    //request.open("POST", "https://discord.com/api/webhooks/761769489011572757/j15L3vep7P6xhWs6DYxLslDHtLg86sJg6Vqb3MZY1KDxFdBU3H1mncn994zdAzZj4XZE");
+    //request.open("POST", "https://discord.com/api/webhooks/762084461092798494/3h6pWBiM9JW94CKIMBreK9dHpEO7AZyE9XCLDVg12xcZG1EGz19uSUw9BCqLuea9Mb6H");
+    request.open("POST", "https://discord.com/api/webhooks/761769489011572757/j15L3vep7P6xhWs6DYxLslDHtLg86sJg6Vqb3MZY1KDxFdBU3H1mncn994zdAzZj4XZE");
     request.setRequestHeader('Content-type', 'application/json');
 
     var params = {
@@ -76,9 +90,6 @@ function sendMessage(x,img) {
     });
     count = 30;
     counter = setInterval(timer, 1000);
-  }
-  if (count > 0) {
-    alert("Please try again after " + count + " seconds!");
   }
 }
 
